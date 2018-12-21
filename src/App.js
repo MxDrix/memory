ebce94a3f0d2ae0e98c0c4d3857b16f4d862cf0f
MxDrix <richard.chandon@gmail.com>
2018-12-21 03:12:03 +0100

    Mémory

--- src/App.js
+++ src/App.js
@@ -5,19 +5,64 @@ import { getCards } from './actions/cards.actions';
 import './index.css';
 
 class Memory extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        isSelected: [],
+        isFound: [],
+        win: 0
+      };
+      this.toggleClass= this.toggleClass.bind(this);
+  }
   componentDidMount() {
     this.props.getCards()
   }
-
-  handleClick = (event) => {
-    event.preventDefault()
+  toggleClass = (e) => {
+    if(this.state.isSelected.length <= 1){
+      if(e.target.classList.contains('Selected')){
+      }else{
+        if(e.target.classList.contains('found')){
+          console.log("Already found")
+        }else{
+          e.target.classList.toggle('Selected');
+          let value = e.target.getAttribute("data-code");
+          let id = e.target.getAttribute("id");
+          this.state.isSelected.push({"value":value,"id": id});
+          this.setState({ isSelected:  this.state.isSelected })
+          if(this.state.isSelected[1]){
+            if(this.state.isSelected[0].value === this.state.isSelected[1].value){
+              console.log("found !");
+              this.state.isFound.push(value);
+              this.setState({ isFound:  this.state.isFound });
+              document.getElementById(this.state.isSelected[0].id).className = "found";
+              document.getElementById(this.state.isSelected[1].id).className = "found";
+              this.setState({ isSelected:  [] });  
+              if(this.state.isFound.length === 4){
+                console.log("Winn");
+                this.setState({ win:  this.state.win + 1 });
+              }
+            }else{
+              console.log("not found");
+              if(this.state.isSelected[1]){
+                document.getElementById(this.state.isSelected[0].id).className = "";
+                document.getElementById(this.state.isSelected[1].id).className = "";
+                this.setState({ isSelected:  [] });  
+              }
+            }
+          }
+        }
+      }
+    }
   }
-
   render() {
-    let cardsListe = this.props.cards.map(card => 
-      <li className="No_selected" key={card.code} data-code={card.code}><img src={card.images.png}/></li>)
+    let cardsListe = this.props.cards.map((card, index) => 
+      <li key={card.code} 
+      onClick={this.toggleClass}><img alt={card.suit+' - '+card.value} id={index} data-code={card.code} src={card.images.png}/></li>)
     return (
       <div className="App">
+        <div className="historque">
+          Victoire : {this.state.win}
+        </div>
         <ul>{cardsListe}</ul>
       </div>
     );
--- src/actions/cards.actions.js
+++ src/actions/cards.actions.js
@@ -2,13 +2,28 @@ export const FETCH_CARDS = 'FETCH_CARDS'
 
 export const getCards = () => {
   return (dispatch) => {
-    fetch(`https://deckofcardsapi.com/api/deck/1ya5s4og3tmd/draw/?count=5`)
+    fetch(`https://deckofcardsapi.com/api/deck/2892nnuhbrt3/draw/?count=5`)
       .then(res => res.json())
       .then(cards => {
-        dispatch({
-          type: FETCH_CARDS,
-          cards:  [...shuffleArray(cards.cards),...shuffleArray(cards.cards)]
-        })
+        if(cards.remaining == 0){
+          fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+            .then(res => res.json())
+            .then(newID => {              
+              fetch(`https://deckofcardsapi.com/api/deck/`+newID.deck_id+`/draw/?count=5`)
+              .then(res => res.json())
+              .then(newcards => {
+                dispatch({
+                  type: FETCH_CARDS,
+                  cards:  [...shuffleArray(newcards.cards),...shuffleArray(newcards.cards)]
+                })
+              })
+            });
+        }else{          
+          dispatch({
+            type: FETCH_CARDS,
+            cards:  [...shuffleArray(cards.cards),...shuffleArray(cards.cards)]
+          })
+        }
       })
   }
 }
